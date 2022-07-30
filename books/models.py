2d1aaf0007bb8094859eb9cac6b5cf0b8613c60d
aaron <aaron@nioyatech.com>
2022-07-30 12:33:17 +0300

    feat: add graphql

--- books/models.py
+++ books/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Publisher(models.Model):
+    id = models.AutoField(primary_key=True)
+    p_name = models.CharField(max_length=40)
+    city = models.CharField(max_length=20)
+    zip = models.CharField(max_length=10)
+    
+    class Meta:
+        db_table = 'Publisher'
+        
+    def __str__(self):
+        return self.p_name
+
+class Book(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    pub = models.ForeignKey('Publisher', models.DO_NOTHING)
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    category = models.CharField(max_length=30, null=True)
+    quantity = models.IntegerField()
+    b_format = models.CharField(max_length=40, null=True)
+    prod_year = models.IntegerField()
+    filesize = models.IntegerField(blank=True, null=True)
+    class Meta:
+        db_table = 'Book'
+        
+    def __str__(self):
+        return self.title
---
+++ books/schema.py
@@ -0,0 +1,27 @@
+import graphene
+from graphene.types import schema
+from graphene_django import DjangoObjectType
+from graphene import ObjectType, Schema
+from .models import Publisher, Book
+
+
+
+class PublisherType(DjangoObjectType):
+    
+    class Meta:
+        model  = Publisher
+        fields = ("id", "p_name", "city", "zip")
+
+
+class BookType(DjangoObjectType):
+    
+    class Meta:
+        model  = Book
+        fields = ("id", "pub", "title", "price", "category", "quantity", "b_format", "prod_year", "filesize")
+        
+        
+class Query(ObjectType):
+    all_book      = graphene.List(BookType)
+    all_publisher = graphene.List(PublisherType)
+
+schema = graphene.Schema(query=Query)
